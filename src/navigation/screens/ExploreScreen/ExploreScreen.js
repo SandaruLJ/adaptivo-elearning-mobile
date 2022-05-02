baf976f054ec013ced80ede2f04903279c6b1a98
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2022-05-02 12:46:55 +0000

    Design greeting header component

--- src/navigation/screens/ExploreScreen/ExploreScreen.js
+++ src/navigation/screens/ExploreScreen/ExploreScreen.js
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "./components/Header";
 import { exploreScreenStyles } from "./ExploreScreen.styles";
 
 export default function ExploreScreen({ navigation }) {
   return (
     <SafeAreaView style={exploreScreenStyles.container}>
-      <Text>Explore Screen</Text>
+      <Header />
     </SafeAreaView>
   );
 }
---
+++ src/navigation/screens/ExploreScreen/components/Header.styles.js
@@ -0,0 +1,51 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+export const headerStyles = StyleSheet.create({
+    container: {
+      height: windowHeight * 25/100,
+      width: '100%',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      borderBottomLeftRadius: windowWidth * 8/100,
+      borderBottomRightRadius: windowWidth * 8/100,  
+    },
+    greetingContainer: {
+      flexDirection: 'row',
+      marginTop: windowWidth * 8/100,
+      marginStart: windowWidth * 8/100,
+    },
+    greeting: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: 'white',
+      marginEnd: 8
+    },
+    subGreeting: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: 'white',
+      marginStart: windowWidth * 8/100,
+    },
+    headerBackground: {
+      flex: 1,
+    },
+    searchbar: {
+      marginTop: windowHeight * 3/100,
+      marginHorizontal: windowWidth * 8/100
+    },
+    avatar: {
+      position: 'absolute',
+      right: windowWidth * 8/100,
+      top: windowHeight * 3/100,
+      backgroundColor: 'grey',
+    },
+    handWave: {
+        height: 32,
+        width: 32
+    }
+  });
+  
