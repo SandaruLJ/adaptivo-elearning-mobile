249f1bd0db9b7fe3f0b62b3687f0de6dd12c621d
Piumi <piumiekanayaka4@gmail.com>
2022-05-24 00:44:55 +0530

    Implement drm and download management

--- android/app/src/main/java/com/sparkcoders/elearningMobile/MainApplication.java
+++ android/app/src/main/java/com/sparkcoders/elearningMobile/MainApplication.java
@@ -7,6 +7,9 @@ import androidx.annotation.NonNull;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.ReactNativeBlobUtil.ReactNativeBlobUtilPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.brentvatne.react.ReactVideoPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -19,6 +22,8 @@ import com.facebook.react.bridge.JSIModulePackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.brentvatne.react.ReactVideoPackage;
+import com.RNFetchBlob.RNFetchBlobPackage; 
 
 public class MainApplication extends Application implements ReactApplication {
   private final ReactNativeHost mReactNativeHost = new ReactNativeHostWrapper(
@@ -35,8 +40,11 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new ReactVideoPackage());
+      
       return packages;
     }
+
 
     @Override
     protected String getJSMainModuleName() {
---
+++ src/components/VideoPlayer/VideoPlayer.styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../utils/colors";
+
+export const videoPlayerStyles = StyleSheet.create({
+    backgroundVideo: {
+        height: 250,
+        width: "100%",
+        backgroundColor: "black",
+      },
+});
